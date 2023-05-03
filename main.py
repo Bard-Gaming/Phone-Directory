1fa75207eae25554d558fe4396d4ffa782422735
Bard-Gaming <christophe@dronne.fr>
2023-05-03 20:27:53 +0200

    e

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import (Flask, render_template)
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='webpages')
+app = Flask(__name__, template_folder='web_pages')
 
 
 def html_file(file_name: str):
-    render_template(f'webpages/{file_name}.html')
+    return render_template(f'{file_name}.html')
 
 # -------------------Routing------------------- :
 
