7674ba9aac46f86e9e03f224419ec0451df6f49b
bob <B)>
2023-05-04 11:55:26 +0200

    Added import_file class for ease of use, improvements, restructured a bit

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__, template_folder='web_pages')
+app = Flask(__name__, template_folder='htmlFiles', static_folder='staticFiles')
 
 
-def html_file(file_name: str):
-    return render_template(f'{file_name}.html')
+# --------------------Tools-------------------- :
+
+class import_file:
+    image_extensions = ['jpg', 'png']
+    static_path = os.path.join('static')
+
+    def __init__(self, file_name, *args, **kargs):
+        self.extension = file_name.split('.')[-1]
+        self.args = args
+        self.kargs = kargs
+
+        temp_dir_args = file_name.split('/') # 'resources/image.png' --> ['resources', 'image.png']
+        self.file_name = os.path.join(*temp_dir_args) # os.path.join('resources', 'image.png')
+
+        self.link = self.render_output()
+
+    def render_output(self):
+        if self.extension == 'html':
+            return render_template(self.file_name, *self.args, **self.kargs)
+
+        if self.extension in self.image_extensions:
+            return os.path.join(self.static_path, self.file_name)
 
 # -------------------Routing------------------- :
 
 @app.route('/')
 def home_page():
-    return html_file('front_page')
+    return import_file('front_page.html').link
--- run server.bat
+++ run server.bat
@@ -2,3 +2,5 @@ set FLASK_APP=main.py
 set FLASK_DEBUG=1
 start Chrome http://localhost:5000/
 python -m flask run
+cd
+PAUSE
