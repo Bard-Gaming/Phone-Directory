34ac8bb7ff9aee1f588bc5465788d96e2aefe467
Bard-Gaming <christophe@dronne.fr>
2023-05-06 20:20:27 +0200

    forgot to commit more; did most of the work

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request #Flask
+from flask_wtf import FlaskForm #WTForms integration
+from wtforms import StringField, SubmitField #WTForms
+from wtforms.validators import DataRequired, Length #WTForms validators
 import os
 
 app = Flask(__name__, template_folder='htmlFiles', static_folder='staticFiles')
-
+app.config['SECRET_KEY'] = '5e97ff184fd17570d846cb2ace1d4992' # clé secrète (requis pour sécurité)
 
 # --------------------Tools-------------------- :
+def read_data(file_name='userdata.txt'):
+    with open(file_name, 'rt', encoding='utf-8') as file:
+        return [data_element.rstrip('\n') for data_element in file.readlines()]
 
+# Outil pour enregistrer données:
+class save_data:
+    def __init__(self, data, file_name='userdata.txt'):
+        self.data = str(data)
+        self.file_name = file_name
+
+    def appendDuplicate(self):
+        with open(self.file_name, 'at', encoding='utf-8') as file:
+            try:
+                file.write(f'{self.data}\n')
+            except Exception as err:
+                print(err)
+
+    def append(self):
+        if self.exists(): # Si données existent déjà -> pas enregistrer
+            print(f' * [{self.__class__}] Failed to save data: Data already saved')
+        else:
+            self.appendDuplicate()
+
+
+    def exists(self, data=None):
+        data = data if data != None else self.data
+        return True if data in read_data(self.file_name) else False
+
+# Outil pour importer les fichiers plus facilement:
 class import_file:
     image_extensions = ['jpg', 'png']
     static_path = os.path.join('static')
@@ -27,8 +58,29 @@ class import_file:
         if self.extension in self.image_extensions:
             return os.path.join(self.static_path, self.file_name)
 
+# -------------Forms (User input)-------------- :
+
+class contactForm(FlaskForm):
+    username = StringField('Nom: ',
+                           validators=[DataRequired(), Length(min=3, max=18)])
+    phone_number = StringField('Numéro de Téléphone: ',
+                               validators=[DataRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Ajouter')
+
 # -------------------Routing------------------- :
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return import_file('front_page.html').link
+    if request.method == 'POST':
+        username = request.form.get('username')
+        phone_number = request.form.get('phone_number')
+
+        save_data((username, phone_number)).append()
+
+    contact_form = contactForm()
+    return import_file('front_page.html', form=contact_form).link
+
+@app.route('/directory/')
+def directory_page():
+    data_list = [eval(data_element) for data_element in read_data()]
+    return import_file('directory_page.html', data_list = data_list).link
---
+++ outils/installation flask.bat
@@ -0,0 +1,5 @@
+rem flask normal:
+python -m pip install flask
+
+rem flask-wtf (Flask/WTForms integration):
+python -m pip install Flask-WTF
