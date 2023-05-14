2e21496eed09686d5cc3886376f5704e11b7de89
Bard-Gaming <christophe@dronne.fr>
2023-05-14 20:34:42 +0200

    aefae

--- staticFiles/javascript/directory_page.js
+++ staticFiles/javascript/directory_page.js
@@ -1,15 +1,15 @@
+function show_contacts() {
+	for (let element of document.getElementsByTagName('li')) {
+		element.style.display = '';
+	}
+}
+
 function contact_lookup(name_lookup) {
 	name_lookup = name_lookup.toLowerCase()
 	show_contacts()
 
 	for (let element of document.getElementsByClassName('nameElement')) {
-		let name = element.innerHTML.substring(5).toLowerCase() // (excludes 'Nom: ')
+		let name = element.innerHTML.substring(5).toLowerCase() // element.innerHTML = 'Nom: Bobby' --> element.innerHTML.lower() = 'nom: bobby' --> element.innerHTML.lower() = 'bobby'
 		name.includes(name_lookup) ? undefined : element.parentElement.style.display = 'none';
-	}
-}
-
-function show_contacts() {
-	for (let element of document.getElementsByTagName('li')) {
-		element.style.display = '';
 	}
 }
